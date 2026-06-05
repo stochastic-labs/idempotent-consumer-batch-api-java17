e515fa7957a115ad75048fd1593fcdc9b355e82c
Lucas Dumer <lucasdumer@gmail.com>
2026-06-05 10:54:38 -0300

    feat input controller

--- src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/interfaces/http/api/v1/InputController.java
+++ src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/interfaces/http/api/v1/InputController.java
@@ -1,4 +1,35 @@
 package com.stochasticlabs.idempotentconsumerbatchapijava17.interfaces.http.api.v1;
 
+import com.stochasticlabs.idempotentconsumerbatchapijava17.application.dto.InputDTO;
+import com.stochasticlabs.idempotentconsumerbatchapijava17.application.usecase.CreateInputUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/idempotent")
+@Tag(name = "Idempotent", description = "Endpoints to input payloads")
 public class InputController {
+
+    private final CreateInputUseCase createInputUseCase;
+
+    public InputController(CreateInputUseCase createInputUseCase) {
+        this.createInputUseCase = createInputUseCase;
+    }
+
+    @Operation(summary = "Process input", description = "Process input")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Payload processed success"),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "500", description = "Failed all strategies")
+    })
+    @PostMapping("/input")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@Validated @RequestBody InputDTO dto) {
+        createInputUseCase.create(dto);
+    }
 }
