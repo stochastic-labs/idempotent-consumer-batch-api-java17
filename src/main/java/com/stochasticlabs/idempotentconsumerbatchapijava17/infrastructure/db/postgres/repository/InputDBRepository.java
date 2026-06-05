8e0e063b9bf72210f11cb2e6b36e4feb9e1a507a
Lucas Dumer <lucasdumer@gmail.com>
2026-06-05 16:49:51 -0300

    feat log in repository

--- src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/infrastructure/db/postgres/repository/InputDBRepository.java
+++ src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/infrastructure/db/postgres/repository/InputDBRepository.java
@@ -3,12 +3,17 @@ package com.stochasticlabs.idempotentconsumerbatchapijava17.infrastructure.db.po
 import com.stochasticlabs.idempotentconsumerbatchapijava17.domain.entity.Input;
 import com.stochasticlabs.idempotentconsumerbatchapijava17.domain.repository.InputRepository;
 import com.stochasticlabs.idempotentconsumerbatchapijava17.infrastructure.db.postgres.entity.InputScheduleEntity;
+import com.stochasticlabs.idempotentconsumerbatchapijava17.interfaces.kafka.InputKafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InputDBRepository implements InputRepository {
 
     private final SpringDataPostgresInputRepository springDataRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(InputKafkaConsumer.class);
 
     public InputDBRepository(SpringDataPostgresInputRepository springDataRepository) {
         this.springDataRepository = springDataRepository;
@@ -19,7 +24,7 @@ public class InputDBRepository implements InputRepository {
         InputScheduleEntity inputScheduleEntity = new InputScheduleEntity(input.integer(), input.status());
 
         InputScheduleEntity inputScheduleEntitySaved = springDataRepository.save(inputScheduleEntity);
-
+        log.info("[input-db-repository-save] Input save success!: {}", inputScheduleEntitySaved.getInteger());
         return new Input(inputScheduleEntitySaved.getInteger(), inputScheduleEntitySaved.getStatus());
     }
 }
