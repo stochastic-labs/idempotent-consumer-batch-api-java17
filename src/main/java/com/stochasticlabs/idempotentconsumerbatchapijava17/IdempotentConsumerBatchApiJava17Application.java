1f430ec60cb07a6dad5c00598e79f4f5b74b9f7d
Lucas Dumer <lucasdumer@gmail.com>
2026-06-06 19:10:24 -0300

    feat env to enable api

--- src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/IdempotentConsumerBatchApiJava17Application.java
+++ src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/IdempotentConsumerBatchApiJava17Application.java
@@ -1,11 +1,23 @@
 package com.stochasticlabs.idempotentconsumerbatchapijava17;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class IdempotentConsumerBatchApiJava17Application {
     public static void main(String[] args) {
-        SpringApplication.run(IdempotentConsumerBatchApiJava17Application.class, args);
+        SpringApplication app = new SpringApplication(IdempotentConsumerBatchApiJava17Application.class);
+
+        String apiEnabled = System.getenv("APP_API_HTTP_ENABLED");
+
+        if ("false".equalsIgnoreCase(apiEnabled)) {
+            app.setWebApplicationType(WebApplicationType.NONE);
+        }
+
+        app.run(args);
+
     }
 }
