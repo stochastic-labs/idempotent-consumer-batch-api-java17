ac9fc17501095a6adf412215814baba244e57cba
Lucas Dumer <lucasdumer@gmail.com>
2026-06-06 19:13:10 -0300

    add find method in repo

--- src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/infrastructure/db/postgres/repository/SpringDataPostgresInputRepository.java
+++ src/main/java/com/stochasticlabs/idempotentconsumerbatchapijava17/infrastructure/db/postgres/repository/SpringDataPostgresInputRepository.java
@@ -1,6 +1,11 @@
 package com.stochasticlabs.idempotentconsumerbatchapijava17.infrastructure.db.postgres.repository;
 
+import com.stochasticlabs.idempotentconsumerbatchapijava17.domain.valueobject.Status;
 import com.stochasticlabs.idempotentconsumerbatchapijava17.infrastructure.db.postgres.entity.InputScheduleEntity;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SpringDataPostgresInputRepository extends JpaRepository<InputScheduleEntity, Long> {}
+public interface SpringDataPostgresInputRepository extends JpaRepository<InputScheduleEntity, Long> {
+    Slice<InputScheduleEntity> findByStatusOrderByIdAsc(Status status, PageRequest pageRequest);
+}
